205cac349b4bc641060b96b9f8806aa656a8f8b1
Kalimaha <guido.barbaglia@gmail.com>
2015-05-04 15:23:56 +0200

    Try/catch block for configuration errors.

--- js/faostat-ui-wide-tables.js
+++ js/faostat-ui-wide-tables.js
@@ -18,10 +18,10 @@ define(['jquery',
             lang_faostat    :   'E',
 
             show_row_code   :   true,
-            row_code        :   'UNFCCCCode',
-            row_label       :   'GUNFItemNameE',
-            cols_dimension  :   'Year',
-            rows_dimension  :   'GUNFItemNameE',
+            row_code        :   null,
+            row_label       :   null,
+            cols_dimension  :   null,
+            rows_dimension  :   null,
 
             prefix          :   'faostat_ui_wide_tables_',
             placeholder_id  :   'faostat_ui_wide_tables'
@@ -35,56 +35,95 @@ define(['jquery',
         /* Extend default configuration. */
         this.CONFIG = $.extend(true, {}, this.CONFIG, config);
 
-        /* Fix the language, if needed. */
-        this.CONFIG.lang = this.CONFIG.lang != null ? this.CONFIG.lang : 'en';
+        /* Check configuration. */
+        try {
 
-        /* Store FAOSTAT language. */
-        this.CONFIG.lang_faostat = Commons.iso2faostat(this.CONFIG.lang);
+            /* Check configuration. */
+            this.check_configuration();
 
-        /* Initiate variables. */
-        var cols_dimension = [];
-        var rows_dimension = [];
-        var codes_buffer = [];
+            /* Fix the language, if needed. */
+            this.CONFIG.lang = this.CONFIG.lang != null ? this.CONFIG.lang : 'en';
 
-        console.log(this.CONFIG.data[0]);
+            /* Store FAOSTAT language. */
+            this.CONFIG.lang_faostat = Commons.iso2faostat(this.CONFIG.lang);
 
-        /* Iterate over the values. */
-        for (var i = 0 ; i < this.CONFIG.data.length ; i++) {
+            /* Initiate variables. */
+            var cols_dimension = [];
+            var rows_dimension = [];
+            var codes_buffer = [];
 
-            /* Store the columns. */
-            if ($.inArray(this.CONFIG.data[i][this.CONFIG.cols_dimension], cols_dimension) < 0) {
-                cols_dimension.push(this.CONFIG.data[i][this.CONFIG.cols_dimension]);
-            }
+            /* Iterate over the values. */
+            for (var i = 0; i < this.CONFIG.data.length; i++) {
 
-            /* Store the rows and the values. */
-            if ($.inArray(this.CONFIG.data[i].UNFCCCCode, codes_buffer) < 0) {
-                codes_buffer.push(this.CONFIG.data[i].UNFCCCCode);
-                var values = [];
-                for (var j = 0 ; j < this.CONFIG.data.length ; j++) {
-                    if (this.CONFIG.data[j].UNFCCCCode == this.CONFIG.data[i].UNFCCCCode) {
-                        values.push(parseFloat(this.CONFIG.data[j].GUNFValue) > -1 ? parseFloat(this.CONFIG.data[j].GUNFValue).toFixed(2) : null);
-                    }
+                /* Store the columns. */
+                if ($.inArray(this.CONFIG.data[i][this.CONFIG.cols_dimension], cols_dimension) < 0) {
+                    cols_dimension.push(this.CONFIG.data[i][this.CONFIG.cols_dimension]);
                 }
-                rows_dimension.push({
-                    values: values,
-                    show_row_code: this.CONFIG.show_row_code,
-                    code: this.CONFIG.data[i][this.CONFIG.row_code],
-                    label: this.CONFIG.data[i][this.CONFIG.row_label]
-                });
+
+                /* Store the rows and the values. */
+                if ($.inArray(this.CONFIG.data[i].UNFCCCCode, codes_buffer) < 0) {
+                    codes_buffer.push(this.CONFIG.data[i].UNFCCCCode);
+                    var values = [];
+                    for (var j = 0; j < this.CONFIG.data.length; j++) {
+                        if (this.CONFIG.data[j].UNFCCCCode == this.CONFIG.data[i].UNFCCCCode) {
+                            values.push(parseFloat(this.CONFIG.data[j].GUNFValue) > -1 ? parseFloat(this.CONFIG.data[j].GUNFValue).toFixed(2) : null);
+                        }
+                    }
+                    rows_dimension.push({
+                        values: values,
+                        show_row_code: this.CONFIG.show_row_code,
+                        code: this.CONFIG.data[i][this.CONFIG.row_code],
+                        label: this.CONFIG.data[i][this.CONFIG.row_label]
+                    });
+                }
+
             }
+
+            /* Load template. */
+            var source = $(templates).filter('#faostat_ui_wide_tables_structure').html();
+            var template = Handlebars.compile(source);
+            var dynamic_data = {
+                cols_dimension: cols_dimension,
+                rows_dimension: rows_dimension,
+                show_row_code: this.CONFIG.show_row_code
+            };
+            var html = template(dynamic_data);
+            $('#' + this.CONFIG.placeholder_id).empty().html(html);
+
+        } catch (e) {
+
+            swal({
+                title: translate.error,
+                type: 'error',
+                text: e,
+                html: true
+            });
 
         }
 
-        /* Load template. */
-        var source = $(templates).filter('#faostat_ui_wide_tables_structure').html();
-        var template = Handlebars.compile(source);
-        var dynamic_data = {
-            cols_dimension: cols_dimension,
-            rows_dimension: rows_dimension,
-            show_row_code: this.CONFIG.show_row_code
-        };
-        var html = template(dynamic_data);
-        $('#' + this.CONFIG.placeholder_id).empty().html(html);
+    };
+
+    WIDE_TABLES.prototype.check_configuration = function() {
+
+        /* Check whether data exists or not. */
+        if (this.CONFIG.data == null)
+            throw translate.data_is_null;
+
+        /* Check row_code. */
+        if (this.CONFIG.show_row_code && this.CONFIG.row_code == null)
+            throw translate.row_code_is_null;
+
+        /* Check row_label. */
+        if (this.CONFIG.row_label == null)
+            throw translate.row_label_is_null;
+
+        /* Check rows_dimension. */
+        if (this.CONFIG.rows_dimension == null)
+            throw translate.rows_dimension_is_null;
+
+        /* Check cols_dimension. */
+        if (this.CONFIG.cols_dimension == null)
+            throw translate.cols_dimension_is_null;
 
     };
 
--- nls/translate.js
+++ nls/translate.js
@@ -1,6 +1,11 @@
 define({
     root : {
-        error: 'FAOSTAT UI Wide Tables Error'
+        error: 'FAOSTAT UI Wide Tables Error',
+        data_is_null: 'Property "data" is null. Please check your configuration.',
+        row_code_is_null: 'Property "row_code" is null. Please check your configuration.',
+        row_label_is_null: 'Property "row_label" is null. Please check your configuration.',
+        cols_dimension_is_null: 'Property "cols_dimension" is null. Please check your configuration.',
+        rows_dimension_is_null: 'Property "rows_dimension" is null. Please check your configuration.'
     },
     fr : true,
     es : true
