2cca95de2d016e46a41c424945434acc99b05f34
Kalimaha <guido.barbaglia@gmail.com>
2015-05-18 15:19:09 +0200

    Configurable "sort by" option.

--- js/faostat-ui-wide-tables.js
+++ js/faostat-ui-wide-tables.js
@@ -25,6 +25,7 @@ define(['underscore',
             cols_dimension                  :   null,
             value_dimension                 :   null,
             bottom_row_codes                :   null,
+            sort_by                         :   'label',
 
             prefix                          :   'faostat_ui_wide_tables_',
             placeholder_id                  :   'faostat_ui_wide_tables',
@@ -135,7 +136,7 @@ define(['underscore',
             });
 
             /* Sort rows by label. */
-            this.CONFIG.template_rows_dimension = _.sortBy(this.CONFIG.template_rows_dimension, 'label');
+            this.CONFIG.template_rows_dimension = _.sortBy(this.CONFIG.template_rows_dimension, this.CONFIG.sort_by);
 
             /* Load template. */
             var source = $(templates).filter('#faostat_ui_wide_tables_structure').html();
